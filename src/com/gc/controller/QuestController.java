faff3aaffea525e6d2832753ea84846e57712242
ZachZatina <zachzatina327@yahoo.com>
2018-03-07 09:56:16 -0500

    added array list maker to quest controller

--- src/com/gc/controller/QuestController.java
+++ src/com/gc/controller/QuestController.java
@@ -1,12 +1,27 @@
 package com.gc.controller;
 
-import org.springframework.web.servlet.ModelAndView;
+import java.util.ArrayList;
 
-import com.gc.controller.dao.HibernateQuestDao;
-import com.gc.model.QuestDTO;
-import com.gc.utils.QuestDao;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.gc.model.Task;
+import com.gc.utils.HibernateUtil;
 
 public class QuestController {
+	
+	public ArrayList<Task> generateQuestList() {
+		SessionFactory sessFact = HibernateUtil.getSessionFactory();
+		Session sess = sessFact.openSession();
+		Transaction tx = sess.beginTransaction();
+		Criteria crit = sess.createCriteria(Task.class);
+		ArrayList<Task> taskList = (ArrayList<Task>) crit.list();
+		tx.commit();
+		sess.close();
+		return taskList;
+	}
 	
 //	@RequestMapping 
 //	public ModelAndView saveQuest(QuestDTO questDto) {
