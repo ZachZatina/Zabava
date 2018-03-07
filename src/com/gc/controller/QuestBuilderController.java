a8e5e2ae018371f3b364fc0959b2dd0105003a2a
Bjorn74 <benfogt@gmail.com>
2018-03-07 18:23:54 -0500

    Admin page controller stuff.

--- src/com/gc/controller/QuestBuilderController.java
+++ src/com/gc/controller/QuestBuilderController.java
@@ -2,35 +2,43 @@ package com.gc.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
+import com.gc.controller.dao.HibernateQuestDao;
+import com.gc.model.QuestDTO;
 import com.gc.model.TaskDTO;
 import com.gc.utils.FourSquareDAOImpl;
 
 @Controller
 public class QuestBuilderController {
-	
-	
+
 	@RequestMapping("builder")
-	public ModelAndView questBuilder(@RequestParam("lat") String lat, @RequestParam("lon") String lon, @RequestParam("questName") String questName, @RequestParam("radius") int radius, @RequestParam("limit") int limit, Model model) throws IOException {
-if (lat.isEmpty()) {
-	lat = "42.335953";
-	lon = "-83.049774";
-}
+	public ModelAndView questBuilder(@RequestParam("lat") String lat, @RequestParam("lon") String lon,
+			@RequestParam("questName") String questName, @RequestParam("radius") int radius,
+			@RequestParam("limit") int limit, Model model) throws IOException {
+		
+		int questID=0;
+		
+		if (lat.isEmpty()) {
+			lat = "42.335953";
+			lon = "-83.049774";
+		}
+		
+		QuestDTO quest = new QuestDTO();
+		quest.setLocation(lat + "," + lon);
+		HibernateQuestDao dao = new HibernateQuestDao();
+		questID = dao.addQuest(quest);
 
-ArrayList<TaskDTO> tasks = new ArrayList<TaskDTO>();
-tasks = FourSquareDAOImpl.getFSvenues(lat,lon,radius,limit);
-		
-		
-		return new ModelAndView("editQuest","tasks",tasks);
+		ArrayList<TaskDTO> tasks = new ArrayList<TaskDTO>();
+		tasks = FourSquareDAOImpl.getFSvenues(lat, lon, radius, limit);
+
+		model.addAttribute("questName", questName);
+		model.addAttribute("questID", questID);
+
+		return new ModelAndView("builder", "tasks", tasks);
 	}
-	
-	
-
 
 }
