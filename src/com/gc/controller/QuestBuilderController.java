efe61a2aca3e9eec7641d47171ea25e07d89b4a9
aicavin <aicavin@gmail.com>
2018-03-07 16:32:09 -0500

    code admin, mapping in QuestBuilderController

--- src/com/gc/controller/QuestBuilderController.java
+++ src/com/gc/controller/QuestBuilderController.java
@@ -11,26 +11,36 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.gc.model.TaskDTO;
 import com.gc.utils.FourSquareDAOImpl;
+import com.gc.utils.GoogleMapsAPICred;
 
 @Controller
 public class QuestBuilderController {
-	
-	
-	@RequestMapping("builder")
-	public ModelAndView questBuilder(@RequestParam("lat") String lat, @RequestParam("lon") String lon, @RequestParam("questName") String questName, @RequestParam("radius") int radius, @RequestParam("limit") int limit, Model model) throws IOException {
-if (lat.isEmpty()) {
-	lat = "42.335953";
-	lon = "-83.049774";
-}
 
-ArrayList<TaskDTO> tasks = new ArrayList<TaskDTO>();
-tasks = FourSquareDAOImpl.getFSvenues(lat,lon,radius,limit);
+	@RequestMapping("admin")
+	public ModelAndView admin(Model model) {
+		// presume this page will be called through submit on a login page
+
+		String mapkey = GoogleMapsAPICred.MAPS_API_KEY;
 		
+		model.addAttribute("mapkey", mapkey);
 		
-		return new ModelAndView("editQuest","tasks",tasks);
+		return new ModelAndView("admin", "message", "questID");
 	}
-	
-	
 
+	@RequestMapping("builder")
+	public ModelAndView questBuilder(@RequestParam("lat") String lat, @RequestParam("lon") String lon,
+			@RequestParam("questName") String questName, @RequestParam("radius") int radius,
+			@RequestParam("limit") int limit, Model model) throws IOException {
+		if (lat.isEmpty()) {
+			lat = "42.335953";
+			lon = "-83.049774";
+		}
+
+//		ArrayList<TaskDTO> tasks = new ArrayList<TaskDTO>();
+//		tasks = FourSquareDAOImpl.getFSvenues(lat, lon, radius, limit);
+
+		String tasks = "tasks";
+		return new ModelAndView("editQuest", "tasks", tasks);
+	}
 
 }
